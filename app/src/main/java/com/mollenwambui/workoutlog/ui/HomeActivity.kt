a08318b03b4db216bfacf1bfbc50f9c786cc35b3
Your Name <mollenwambui@gmail.com>
2022-09-01 18:53:57 +0300

    Fragments

--- app/src/main/java/com/mollenwambui/workoutlog/ui/HomeActivity.kt
+++ app/src/main/java/com/mollenwambui/workoutlog/ui/HomeActivity.kt
@@ -1,9 +1,8 @@
-package com.mollenwambui.workoutlog
+package com.mollenwambui.workoutlog.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentContainerView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mollenwambui.workoutlog.R
 import com.mollenwambui.workoutlog.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,27 +11,25 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        castViews()
+
         setupBottomnav()
     }
-    fun castViews(){
 
-    }
     fun setupBottomnav(){
-       binding.btmHome.setOnItemSelectedListener { item->
+       binding.bnvHome.setOnItemSelectedListener { item->
           when(item.itemId) {
               R.id.plan ->{
                   var transaction=supportFragmentManager.beginTransaction()
-                  transaction.replace(R.id.fcvhome,PlanFragment())
+                  transaction.replace(R.id.fcvhome, PlanFragment())
                   transaction.commit()
                   true
               }
               R.id.track ->{
-                  supportFragmentManager.beginTransaction().replace(R.id.fcvhome,TrackFragment()).commit()
+                  supportFragmentManager.beginTransaction().replace(R.id.fcvhome, TrackFragment()).commit()
                   true
               }
-              R.id.profile->{
-                  supportFragmentManager.beginTransaction().replace(R.id.fcvhome,ProfileFragment()).commit()
+              R.id.profile ->{
+                  supportFragmentManager.beginTransaction().replace(R.id.fcvhome, ProfileFragment()).commit()
                   true
               }
               else->false
